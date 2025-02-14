2a37739e237caaef1ad5c717fcb7eb280b51e35d
LiviaMacedo30 <anna.livia.santos.macedo.costa@ccc.ufcg.edu.br>
2025-02-14 20:26:24 -0300

    Sua mensagem de commit aqui

---
+++ SelectionSort.java
@@ -0,0 +1,41 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // Encontra o menor elemento no restante do array
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Troca o menor elemento encontrado com o primeiro elemento não ordenado
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        
+        System.out.println("Array antes da ordenação:");
+        printArray(arr);
+
+        selectionSort(arr);
+        
+        System.out.println("Array após a ordenação:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
+
